6b72d84d6062502bc0ab84a1cb7e17b6e197dfb4
杨劼 <yangjie123098@live.cn>
2023-06-13 09:42:38 +0800

    .

--- Sample/Avalonia.ImageBufferView.Sample/Program.cs
+++ Sample/Avalonia.ImageBufferView.Sample/Program.cs
@@ -1,5 +1,4 @@
-﻿using Avalonia;
-using Avalonia.ReactiveUI;
+﻿using Avalonia.ReactiveUI;
 using System;
 
 namespace Avalonia.ImageBufferView.Sample
--- Sample/Avalonia.ImageBufferView.Sample/ViewModels/MainWindowViewModel.cs
+++ Sample/Avalonia.ImageBufferView.Sample/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ReactiveUI;
 
 namespace Avalonia.ImageBufferView.Sample.ViewModels
 {
